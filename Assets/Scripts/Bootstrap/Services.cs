896f4733f7b8be95b3119eb43d7aeaa49ab5d92f
lior_dd <44544953+gothHokage@users.noreply.github.com>
2026-04-25 19:22:48 +0300

    lil upd

--- Assets/Scripts/Bootstrap/Services.cs
+++ Assets/Scripts/Bootstrap/Services.cs
@@ -1,16 +1,7 @@
 using UnityEngine;
 
-public class Services : MonoBehaviour
+public static class Services 
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
+    
 }
